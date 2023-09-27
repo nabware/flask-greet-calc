754fbdbfd1b7c14f25f3c3f30c484929aa779ca2
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-27 12:09:07 -0700

    Completed calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.get("/add")
+def add_handler():
+    """Returns a + b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return str(add(a, b))
+
+@app.get("/sub")
+def sub_handler():
+    """Returns a - b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return str(sub(a, b))
+
+@app.get("/mult")
+def mult_handler():
+    """Returns a * b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return str(mult(a, b))
+
+@app.get("/div")
+def div_handler():
+    """Returns a / b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return str(div(a, b))
--- greet/app.py
+++ greet/app.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 @app.get("/welcome")
 def welcome():
     """Returns welcome"""
+
     return "welcome"
 
 @app.get("/welcome/home")
 def welcome_home():
     """Returns welcome home"""
+
     return "welcome home"
 
 @app.get("/welcome/back")
 def welcome_back():
     """Returns welcome back"""
+
     return "welcome back"
