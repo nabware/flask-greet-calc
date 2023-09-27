acb3b8deb323bfff2a972cb0b59cd066be6bacfd
JaeWon Jin <j1jin286@gmail.com>
2023-09-27 12:21:47 -0700

    Completed Further Study

--- calc/app.py
+++ calc/app.py
@@ -38,3 +38,15 @@ def div_handler():
     b = int(request.args.get("b"))
 
     return str(div(a, b))
+
+@app.get("/math/<operation>")
+def math_handler(operation):
+    """Return result of math operation of a and b"""
+    operations = {"add": add, "sub": sub,
+                  "mult": mult, "div": div}
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    funct = operations.get(operation)
+    return str(funct(a,b))
